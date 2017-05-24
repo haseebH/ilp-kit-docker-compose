13211975ebfc9f3904eda8f33c4c941aa1c2010d
haseeb <haseeb@platalytics.com>
2017-05-24 21:32:01 +0500

    added public domain name and security group functionality

--- azure/azure-deploy.sh
+++ azure/azure-deploy.sh
@@ -1,25 +1,9 @@
-curl -fsSL https://apt.dockerproject.org/gpg | sudo apt-key add -
-apt-key fingerprint 58118E89F3A912897C070ADBF76221572C52609D
-sudo add-apt-repository \
-       "deb https://apt.dockerproject.org/repo/ \
-       ubuntu-$(lsb_release -cs) \
-       main"
-sudo apt-get update
-sudo apt-get -y install docker-engine
-
-sudo gpasswd -a ${USER} docker
-sudo service docker restart   
-
-
-sudo curl -L "https://github.com/docker/compose/releases/download/1.10.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-
 sudo apt-get install -y git
 cd $4
-git clone https://github.com/interledgerjs/ilp-kit-docker-compose
+git clone https://github.com/adrianhopebailie/ilp-kit-docker-compose.git
 #Set ENV Variables here
 sudo apt-get install -y openssl
-password=$(openssl rand -base64 32 | cut -c1-32)
+password=$(openssl rand -base64 32 | cut -c1-32|tr "/" +)
 touch /etc/motd
 echo "***********************************************"\
    " Your password for ILP is $password"\
